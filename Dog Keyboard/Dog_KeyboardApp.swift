4d9d68feb67e79b6b5509663a0caf0e0c2b8ceae
Johnny Archard <jla1628@jagmail.southalabama.edu>
2022-11-21 01:46:32 -0600

    Initial Commit

---
+++ Dog Keyboard/Dog_KeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dog_KeyboardApp.swift
+//  Dog Keyboard
+//
+//  Created by Johnny Archard on 11/21/22.
+//
+
+import SwiftUI
+
+@main
+struct Dog_KeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
