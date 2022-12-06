59ae3f5bd5aeea97a2891312a2de810d1cd6a4b8
Johnny Archard <jla1628@jagmail.southalabama.edu>
2022-12-06 13:52:29 -0600

    Initial Commit

---
+++ Frontend/FrontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrontendApp.swift
+//  Frontend
+//
+//  Created by Johnny Archard on 12/6/22.
+//
+
+import SwiftUI
+
+@main
+struct FrontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
